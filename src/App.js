4a958be6ca4223d6cc8534c749f36c759346c756
SoyTiyi <santiago.marmartinez@gmail.com>
2021-03-02 09:41:53 -0500

    add register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Login from './Component/login/Login';
+import Register from './Component/register/Register'
+import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Switch>
+      <Route path="/" component={Login} exact/>
+      <Route path="/register" component={Register}/>
+    </Switch>
   );
 }
 
