dddd22adf20901d148a71851e901671238ee51b8
SoyTiyi <santiago.marmartinez@gmail.com>
2021-03-02 10:37:29 -0500

    add register page

--- src/Component/login/Login.js
+++ src/Component/login/Login.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+
 const Login = props => {
+    const history = useHistory();
+    const redirectRegister = () => {
+        history.push("/register");
+    }
+
     return (
         <div className="w-full h-screen flex">
             <img src="https://wallpapercave.com/wp/wp2056219.jpg" alt="background" class="object-cover object-center h-screen w-7/12" />
@@ -10,7 +17,12 @@ const Login = props => {
                         className="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded" />
                     <input type="password" name="password" placeholder="password" autocomplete="off"
                         className="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded" />
-                    <button className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-lg focus:outline-none shadow">Sign In</button>
+                    <div className="inline-flex">
+                    <button className="bg-red-500 hover:bg-red-600 text-gray-800 font-bold py-2 px-4 rounded-l">Sign In</button>
+                    <button className="bg-red-500 hover:bg-red-600 text-gray-800 font-bold py-2 px-4 rounded-r"
+                        onClick={redirectRegister}>Register</button>
+                    </div>
+                    
                 </div>
             </div>
         </div>
--- src/Component/register/Register.js
+++ src/Component/register/Register.js
@@ -2,54 +2,51 @@ import React from 'react';
 
 const Register = props => {
     return (
-        <div class="bg-grey-lighter min-h-screen flex flex-col">
-            <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 class="mb-8 text-3xl text-center">Sign up</h1>
-                    <input
-                        type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="Full Name" />
+        <div class="w-full h-screen flex">
+            <img src="https://wallpapercave.com/wp/wp4507164.jpg" alt="background" class="object-cover object-center h-screen w-10/12" />
+            <div class="bg-gray-700 min-h-screen flex flex-col">
+                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div class="bg-gray-500 px-6 py-8 rounded shadow-md text-black w-full">
+                        <h1 class="mb-8 text-3xl text-center text-red-600">Sign up</h1>
+                        <input
+                            type="text"
+                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="fullname"
+                            placeholder="Full Name" />
 
-                    <input
-                        type="text"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
+                        <input
+                            type="text"
+                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="email"
+                            placeholder="Email" />
 
-                    <input
-                        type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
-                    <input
-                        type="password"
-                        class="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="confirm_password"
-                        placeholder="Confirm Password" />
+                        <input
+                            type="password"
+                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="password"
+                            placeholder="Password" />
+                        <input
+                            type="password"
+                            class="block border border-grey-light w-full p-3 rounded mb-4"
+                            name="confirm_password"
+                            placeholder="Confirm Password" />
 
-                    <button
-                        type="submit"
-                        class="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Create Account</button>
+                        <button
+                            type="submit"
+                            class="w-full text-center py-3 rounded bg-red-600 text-white hover:bg-red-700 focus:outline-none my-1"
+                        >Create Account</button>
 
-                    <div class="text-center text-sm text-grey-dark mt-4">
-                        By signing up, you agree to the
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                            Terms of Service
-                        </a> and
-                        <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                            Privacy Policy
-                        </a>
+                        <div class="text-center text-sm text-grey-dark mt-4">
+                            Vive la experiencia de compartir tu pasión por el Crossfit con personas que te entenderán
+                        </div>
                     </div>
-                </div>
 
-                <div class="text-grey-dark mt-6">
-                    Already have an account?
-                    <a class="no-underline border-b border-blue text-blue" href="../login/">
-                        Log in
-                    </a>.
+                    <div class="text-grey-dark mt-6">
+                        ¿Ya tienes cuenta?
+                    <a class="no-underline border-b border-blue-500 text-blue-500" href="../login/">
+                        Login
+                    </a>
+                </div>
                 </div>
             </div>
         </div>
