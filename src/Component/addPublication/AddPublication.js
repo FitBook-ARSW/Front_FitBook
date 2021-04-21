1cf52f2382d1d0197007f603b1ab4bf6d44e60f9
javier200151 <javier200151@gmail.com>
2021-04-21 16:10:52 -0500

    Likes

--- src/Component/addPublication/AddPublication.js
+++ src/Component/addPublication/AddPublication.js
@@ -71,6 +71,7 @@ const AddPublication = props => {
                 </div>
             </div>
         </div>
+
     );
 }
 
--- src/Component/publication/Publication.js
+++ src/Component/publication/Publication.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import StarIcon from '@material-ui/icons/Star';
 
 const Publication = props => {
+    const [likes,setLikes] = useState(false);
+    const addlikes= () => {
+        setLikes(!likes);
+        console.log(likes);
+    };
+    
     return (
         <div className="min-h-screen min-w-screen bg-white flex items-center justify-center">
             <div>
@@ -10,13 +18,18 @@ const Publication = props => {
                         <img className="w-full rounded-md" src="https://coffeeordie.com/wp-content/uploads/2019/03/FraserCOVER2.jpg" alt="motivation" />
                         : <img className="w-full rounded-md" src={props.imgUrl} alt="motivation" />
                     }
+                    <p className="text-center">
+                    <IconButton onClick={() => addlikes() } color={likes? 'delete' : 'secondary'} aria-label="add to shopping cart">
+                        <StarIcon />
+                    </IconButton>
+                    </p>
                     <p className="text-center leading-relaxed">{props.content}</p>
-                    <p className="text-center leading-relaxed">Likes: {props.likes}</p>
                     <p className="text-center leading-relaxed">{props.uploaDate}</p>
                 </div>
             </div>
         </div>
     );
+    
 }
 
 export default Publication;
